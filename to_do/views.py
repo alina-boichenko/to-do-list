e99afb396cf7ae8329b433391a6d0a52a25b0ef0
Alina Boichenko <ilub1108@gmail.com>
2023-04-24 16:38:20 +0300

    Created TagListView

--- to_do/views.py
+++ to_do/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from to_do.models import Task
+from to_do.models import Task, Tag
 
 
 class TaskListView(ListView):
     model = Task
     template_name = "to_do/task_list.html"
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = "to_do/tag_list.html"
