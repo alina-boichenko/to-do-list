3953409235309b29360d69bd28f14e5af8181c98
Alina Boichenko <ilub1108@gmail.com>
2023-04-24 16:37:50 +0300

    Added path to TagListView

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from to_do.views import TaskListView
-
+from to_do.views import TaskListView, TagListView
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task-list")
+    path("", TaskListView.as_view(), name="task-list"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "to_do"
