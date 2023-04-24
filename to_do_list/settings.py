c7bec1c5014f590ce4faa81b7bc04df9220510ea
Alina Boichenko <ilub1108@gmail.com>
2023-04-24 15:57:34 +0300

    Added STATICFILES_DIRS

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "to_do_list.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
+        "DIRS": [BASE_DIR / "templates"]
         ,
         "APP_DIRS": True,
         "OPTIONS": {
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = (BASE_DIR / "static",)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
