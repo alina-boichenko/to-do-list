0e825ed3c5aec6e641b6957f857afea3b8ac7b3a
Alina Boichenko <ilub1108@gmail.com>
2023-04-25 01:01:03 +0300

    Created path to TaskCreateView

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from to_do.views import TaskListView, TagListView
+from to_do.views import (
+    TaskListView,
+    TaskCreateView,
+    TagListView,
+)
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("tasks-create/", TaskCreateView.as_view(), name="task-create"),
     path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
