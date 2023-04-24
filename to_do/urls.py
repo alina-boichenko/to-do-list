5538f238ec394d3c32902d376807247c10070e5f
Alina Boichenko <ilub1108@gmail.com>
2023-04-25 01:28:57 +0300

    Added path to TagCreateView

--- to_do/urls.py
+++ to_do/urls.py
@@ -4,12 +4,14 @@ from to_do.views import (
     TaskListView,
     TaskCreateView,
     TagListView,
+    TagCreateView,
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("tasks-create/", TaskCreateView.as_view(), name="task-create"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
 ]
 
 app_name = "to_do"
