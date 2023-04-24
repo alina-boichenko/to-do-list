9b1865611db1e0d1b1a3ca7505c75c70ce11c174
Alina Boichenko <ilub1108@gmail.com>
2023-04-24 16:07:33 +0300

    Created TaskListView

--- to_do/views.py
+++ to_do/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from to_do.models import Task
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = "to_do/task_list.html"
