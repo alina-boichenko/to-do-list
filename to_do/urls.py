a43e148e8a80f03974a5a691d7da568ad64bdca6
Alina Boichenko <ilub1108@gmail.com>
2023-04-25 01:51:25 +0300

    Added path to TaskUpdateView

--- to_do/urls.py
+++ to_do/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from to_do.views import (
     TaskListView,
     TaskCreateView,
+    TaskUpdateView,
     TagListView,
     TagCreateView,
 )
@@ -10,6 +11,7 @@ from to_do.views import (
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("tasks-create/", TaskCreateView.as_view(), name="task-create"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
 ]
