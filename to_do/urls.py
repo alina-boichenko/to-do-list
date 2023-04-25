ad700eff5d9e46af1043dcfd4bbb7f1122e7b53c
Alina Boichenko <ilub1108@gmail.com>
2023-04-25 12:23:14 +0300

    Added path to TagDeleteView

--- to_do/urls.py
+++ to_do/urls.py
@@ -8,6 +8,7 @@ from to_do.views import (
     TagListView,
     TagCreateView,
     TagUpdateView,
+    TagDeleteView,
 )
 
 urlpatterns = [
@@ -18,6 +19,7 @@ urlpatterns = [
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "to_do"
